56177cb1803dcc781e9967ff6040ad9af7e43e5d
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-20 10:45:36 +0530

    cm

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Todo() {
 
 
-     const [todos,setTodo] = useState([])   //todos =[] if any change in value of useState setTodo() apply it in todos
+  const [todos, setTodo] = useState([])   //todos =[] if any change in value of useState setTodo() apply it in todos
+
+  useEffect(() => {
+
+    fetch("https://dummyjson.com/todos").then(res => res.json()).then(data => {
+
+      setTodo(data.todos);
+
+
+
+    })
+  },[])
+
+
+
+
   return (
     <div>
-        <h1>Todo List</h1>
-        <p>Total Todos: {todos.length}</p>
+      <h1>Todo List</h1>
+      <p>Total Todos: {todos.length}</p>
+      <ul>{todos.map(todo => {
+        <li><span>{todo.todo}</span><span>{todo.completed}</span></li>
+
+      })}</ul>
     </div>
   )
 }
@@ -16,5 +35,14 @@ export default Todo
 
 
 
-// state:(its like a variable) page didnot reload 
+// state:(its like a variable) page didnot reload
 // state: if the data changes in the js, in React UI will change it automatically using state
+// useEffect :
+
+
+// fetch("url").(res => res.json()).then(data =>{
+//         setTodo(data.todos);
+//          })
+
+
+//fetch has two attributes : then and promise
